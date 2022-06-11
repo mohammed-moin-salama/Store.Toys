ad63a99a73e330a285405f83eb84ebaf744116cf
Mohammed Salama <107002018+mohammed-moin-salama@users.noreply.github.com>
2022-06-11 17:47:27 +0300

    Add files via upload

---
+++ js/list2.js
@@ -0,0 +1,18 @@
+﻿var list2 = [
+    { details: 1, id: 11, name: 'Happy Flower', price: ' 38.00', img: '5baf529c7a16ad5b5fd9fdf3_33727-9-wooden-toy-transparent-image-min.png' },
+    { details: 2, id: 12, name: 'Lift Machine', price: ' 24.00 ', img: '5baf525bbf02340f30398cb3_33505-6-wooden-toy-clipart-min.png' },
+    { details: 3, id: 13, name: 'Wooden Camera', price: '32.00', img: '5baf522457091399591a83fe_33631-9-wooden-toy-photo-min (1).png' },
+    { details: 4, id: 14, name: 'Little Rabbit', price: '16.00', img: '5baf51fc570913c1d31a83f6_33504-4-wooden-toy-transparent-min.png' },
+    { details: 5, id: 15, name: 'Rainbow Truck', price: '28.00', img: '5baf51b7c8d851a73cc4544c_33649-6-toy-transparent-image-min.png' },
+    { details: 6, id: 16, name: 'Happy Dog', price: '30:00', img: '5baf5171ace69cb064b33d42_33388-1-wooden-toy-photos-min-p-500.png' },
+    { details: 7, id: 17, name: 'Caterpillar', price: '18.00', img: '5baf50843a685ea7dfd6e4fd_33371-3-wooden-toy-min-p-500.png' },
+    { details: 8, id: 18, name: 'Wooden Tractor', price: '22.00', img: '5baf5100ace69c1b26b33d2d_33476-9-wooden-toy-hd-min-p-800.png' },
+    { details: 9, id: 8, name: 'Mega Plush Toy', price: '38.00', img: '5bae124e03ef144f2b4a9bef_33837-2-plush-toy-transparent-background-min.png' },
+    { details: 10, id: 9, name: 'Teddy Bear', price: '30:00', img: '5bae12942ca03553bf0d536c_33903-2-plush-toy-transparent-image-min.png' },
+    { details: 11, id: 7, name: 'Cute Dog', price: '24.00', img: '5bae0f7a1f2da258291736c4_33908-7-plush-toy-file-min.png' },
+    { details: 12, id: 6, name: 'Little Friend', price: '27.00', img: '5bae0f1835e11376299a8089_33878-5-plush-toy-transparent-min.png' },
+    { details: 13, id: 5, name: 'Christmas Deer', price: '19.00', img: '5bae0ede56ac5481f54e12e9_33610-4-plush-toy-hd-min-p-500.png' },
+    { details: 14, id: 4, name: 'Pluto Yellow Dog', price: '28.00', img: '5bae0e6335e113da999a7976_33568-4-toy-image-min-p-500.png' },
+    { details: 15, id: 3, name: 'Grey Elephant', price: '45.00', img: '5bae0db61f2da2a4ef173617_cute-plush-toy-stuffed-animal-47335-min-p-500.png' },
+    { details: 16, id: 2, name: 'Funny Clown', price: '36.00', img: '5bae0d07939555eac3b8a91c_33910-6-toy-clipart-min-p-500.png' }
+];
---
+++ js/login.js
@@ -0,0 +1,61 @@
+﻿var use = 1;
+var pas = 1;
+var list = ['1', '2', localStorage.getItem(use), listt];
+var list2 = ['moh', 'moh', localStorage.getItem(pas), list22];
+
+
+
+
+
+
+
+    var form = document.getElementById('form');
+    var btnclick = document.getElementById('btn');
+    var frmNews = document.getElementById('frmNews');
+    var newsDiv = document.getElementById('newsDiv');
+    form.addEventListener('submit', login);
+    btnclick.addEventListener('click', btn);
+    frmNews.addEventListener('submit', add);
+    function login(event) {
+            if (!form.checkValidity()) {
+        event.preventDefault();
+    event.stopPropagation();
+            }
+    else {
+        form.classList.add('was-validated');
+
+    var id = list.indexOf(form.elements.user.value);
+    var name = list2.indexOf(form.elements.pass.value);
+    if (id == name && id != -1 && name!=-1) {
+        window.location = 'index.html';
+    event.preventDefault();
+    event.stopPropagation();
+                }
+    else {
+        alert('please inter user again')
+    }
+            }
+            
+        }
+    function add(event) {
+            if (!frmNews.checkValidity()) {
+        event.preventDefault();
+    event.stopPropagation();
+            }
+    frmNews.classList.add('was-validated');
+    localStorage.setItem('use',frmNews.elements.id.value);
+    localStorage.setItem('pas', frmNews.elements.useradd.value);
+
+
+
+
+    form.classList.add('d-block');
+
+        }
+    function btn(event) {
+        form.classList.add('d-none');
+    newsDiv.classList.toggle('d-none');
+    btnclick.classList.toggle('d-none');
+
+        }
+
